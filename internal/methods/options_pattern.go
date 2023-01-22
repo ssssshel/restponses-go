57f60dc15ba4f4753e18ca7c8f96136427186342
ssssshel <angelarteaga989@gmail.com>
2023-01-22 13:34:14 -0500

    bases interfaces clean

--- internal/methods/options_pattern.go
+++ internal/methods/options_pattern.go
@@ -2,8 +2,6 @@ package methods
 
 import "github.com/ssssshel/restponses-go/internal/interfaces"
 
-type BaseResponseOpt func(*interfaces.GeneralInput)
-
 type Response2xxOpt func(*interfaces.GenericSuccessfullResponse)
 
 type Response3xxOpt func(*interfaces.GenericRedirectionResponse)
